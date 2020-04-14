e690e93e2393aa9e52f6736c4cb26bcf95d3311e
Szymon Pulut <szymon.pulut@gmail.com>
2020-04-14 17:50:41 +0200

    Initial commit

---
+++ web-build/precache-manifest.0ee6d92e488d01f44c03e9fd47b7c587.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb7aa8d46e9ce306aad19abb059a713",
+    "url": "/static/media/tile_tap.dcb7aa8d.wav"
+  },
+  {
+    "revision": "54e2d328404df857a1ad",
+    "url": "/static/js/app.8a05c57b.chunk.js"
+  },
+  {
+    "revision": "bc60ff0b3f3f15d285f4c3263ebc0afc",
+    "url": "/static/media/dead.bc60ff0b.png"
+  },
+  {
+    "revision": "efafed6083a2da566857871dc879df4b",
+    "url": "/static/media/trophy.efafed60.png"
+  },
+  {
+    "revision": "87624f77c4af1c5efef9de409f08293a",
+    "url": "/static/media/button.87624f77.wav"
+  },
+  {
+    "revision": "715a71a1dbf0fe89b56522825b969b40",
+    "url": "/static/media/play_arrow.715a71a1.png"
+  },
+  {
+    "revision": "cf59a985525474418fb5def657d25330",
+    "url": "/static/media/leaderboard.cf59a985.png"
+  },
+  {
+    "revision": "e15576adeaa8ac372ac8e2d3889512b3",
+    "url": "/static/media/speaker-on.e15576ad.png"
+  },
+  {
+    "revision": "22daf4c904c8252398b311913f3dc75c",
+    "url": "/static/media/speaker-off.22daf4c9.png"
+  },
+  {
+    "revision": "42cec8b325815ff8c8197901c3c42a36",
+    "url": "/static/media/Komiku_Mushrooms.42cec8b3.mp3"
+  },
+  {
+    "revision": "5efeb52c22b38cd6a39d2834e1ebd60c",
+    "url": "/static/media/pause.5efeb52c.png"
+  },
+  {
+    "revision": "05313e56cc0607034777b136a6f8f26f",
+    "url": "/static/media/play.05313e56.png"
+  },
+  {
+    "revision": "49e2a7711c627fbf3df61477079dbc3d",
+    "url": "/static/media/replay.49e2a771.png"
+  },
+  {
+    "revision": "75e2e6977e746838521f361c5a00b1f7",
+    "url": "/static/media/clock.75e2e697.png"
+  },
+  {
+    "revision": "60522ecbb1d3a0da6397c65b17319490",
+    "url": "/static/media/Komiku_BattleOfPogs.60522ecb.mp3"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "ef02fbb4d63089bd67f49df7bd19908f",
+    "url": "/static/media/tile_wrong.ef02fbb4.wav"
+  },
+  {
+    "revision": "3cfb033753254a3661503d836c4a272b",
+    "url": "/static/media/pause_in.3cfb0337.wav"
+  },
+  {
+    "revision": "a225be5b8f4db6f142eaefb47062b48e",
+    "url": "/static/media/pause_out.a225be5b.wav"
+  },
+  {
+    "revision": "b8d1ac9128a6611031a8bdc1b18b6cba",
+    "url": "/static/media/lose.b8d1ac91.wav"
+  },
+  {
+    "revision": "bde7fcbc76d7bd53894886d5f245a231",
+    "url": "/static/media/mug.bde7fcbc.png"
+  },
+  {
+    "revision": "5258575804bb6b57cf25",
+    "url": "/static/js/2.64f874e9.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "da4b2cf98f957662d0038851d870cf20",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a64c65846cf83fab04aa6f3d134e826b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "677f8bb4a4045fb836871f3726396c34",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "981abb9ca8103e03aba28383700ae40d",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "bd68d62da90e2691f6a8296ead109729",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  }
+];
